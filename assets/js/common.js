f73c121e5f7dfd4e05dd478d35d453752862784d
Hideo Masuda <hideomasuda0618@gmail.com>
2025-02-06 23:50:03 +0900

    ポートフォリオ　改修

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,7 +3,7 @@ $(function () {
   $('a[href^="#"]').click(function () {
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
+    var position = target.offset().top - 100;
     var speed = 500;
     $("html, body").animate({
       scrollTop: position
